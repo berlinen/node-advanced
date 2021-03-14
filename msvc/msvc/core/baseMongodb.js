3f42f0056fe72e3a7de08db9473a04aac5b75509
berlinen <1292393108@qq.com>
2021-03-14 15:05:42 +0800

    feat: msvc;

---
+++ msvc/msvc/core/baseMongodb.js
@@ -0,0 +1,36 @@
+/**
+ * @description 封装 mongodb api
+ */
+
+const uri = "mongodb://berlin:berlin0211@cluster0-shard-00-00.jioi1.mongodb.net:27017,cluster0-shard-00-01.jioi1.mongodb.net:27017,cluster0-shard-00-02.jioi1.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-whlnqq-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+const MongoClient = require('mongodb').MongoClient;
+
+let baseMongodb;
+
+class BaseMongodb {
+    constructor() {
+        this.mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        this.mongoClient.connect(err => {
+            if(err){
+                console.log('connect db error', err);
+                return;
+            }
+            this.client = this.mongoClient;
+        });
+    }
+
+    async getClient() {
+        if(!this.client) {
+            this.client = await this.mongoClient.connect();
+        }
+        return this.client;
+    }
+}
+
+ module.exports = () => {
+     if(!baseMongodb){
+        baseMongodb = new BaseMongodb();
+     }
+     return baseMongodb;
+ }
---
+++ msvc/msvc/util/index.js
@@ -0,0 +1,48 @@
+const baseMongodb = require("../core/baseMongodb")();
+
+/**
+ *
+ * @description 设置响应数据
+ * @param object res http res
+ * @param boolean ret boolean
+ * @param string message string
+ * @param object dataInfo object
+ * @param int httpStatus
+ */
+function setResInfo({res, ret, message, dataInfo, httpStatus = 200}) {
+  let retInfo = {};
+  if(!ret) {
+    retInfo = {
+      ret: -1,
+      message: message ? message : 'error',
+      data: {}
+    };
+  } else {
+    retInfo = {
+      ret: 0,
+      message: message ? message : 'success',
+      data: dataInfo ? dataInfo : {}
+    }
+  }
+  res.writeHead(httpStatus, { 'Content-Type': 'text/plain' });
+  res.write(JSON.stringify(retInfo));
+  res.end();
+  return;
+}
+/**
+ *
+ * @description db 数据查询
+ * @param object queryOption
+ */
+async function queryData (queryOption) {
+  const client = await baseMongodb.getClient();
+  const collection = client.db('berlin').collection('user');
+  const queryArr = await collection.find(queryOption).toArray();
+
+  return queryArr;
+};
+
+module.exports = {
+  setResInfo,
+  queryData
+}
