e3181f9ecd96acaa544cd71cd598a855a9a121b5
berlinen <1292393108@qq.com>
2021-04-03 13:55:38 +0800

    chore: cpuoverload

--- cpu/cpuOverload.js
+++ cpu/cpuOverload.js
@@ -2,6 +2,7 @@ const platform = process.platform;
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const os = require('os');
+const { pid } = require('process');
 
 let overloadTimes = 0; // 指 CPU 过载持续次数
 let isOverload = false;
@@ -90,11 +91,63 @@ class CpuOverload {
   }
 
   /**
-   *
+   * @description 获取进程信息
    */
    async _getProcessInfo () {
+    let pidInfo, cpuInfo;
 
+    if(platform === 'win32') {
+      pidInfo = await this._getWmic();
+    } else {
+      pidInfo = await this._getPs();
+    }
+
+    cpuInfo = await this._parseInOs(pidInfo);
+    // 异常处理
+    if(!cpuInfo || cpuInfo === '') {
+      return false;
+    }
+    //  命令行数据，字段解析处理
+    console.log(parseFloat(cpuInfo));
+    return parseFloat(cpuInfo).toFixed(4);
    }
+
+  /**
+   * @description 使用 ps 命令获取进程信息
+   */
+  async _getPs () {
+    // cmd
+    const cmd = `ps -p ${process.pid} -o pcpu`;
+
+    // 获取执行结果
+    const { stdout, stderr } = await exec(cmd);
+
+    if(stderr) return err;
+
+    return stdout;
+  }
+
+  _getWmic () {
+    const cols = 'IDProcess,Name,PercentProcessorTime,PrivateBytes,VirtualBytes';
+    const cmd  = 'wmic path Win32_PerfFormattedData_PerfProc_Process get ' + cols + ' /format:csv';
+
+    const { stdout, stderr } = await exec(cmd);
+    console.log(stdout);
+    return stdout;
+  }
+
+  async _parseInOs(pidInfo) {
+    let lines = pidInfo.trim().split(os.EOL);
+    if(!lines || lines.length < 2) {
+      return false;
+    }
+    let cpuStr = lines[1];
+    return cpuStr.trim();
+  }
+
+  async _parseInWin() {
+
+  }
 
    /**
     * @description 获取丢弃概率
@@ -104,7 +157,6 @@ class CpuOverload {
     let c = currentCpuPercentage >= 100 ? 10 : currentCpuPercentage/10; // 指 CPU 当前负载越高，占用率越大则丢弃概率越大，
     let b = this.baseProbability;  // 用 b 表示，是负载最大时的丢弃概率，取值范围为 0 ~ 1。
     currentProbability = ((0.1 * o) * Math.exp(c) / maxValue * b).toFixed(4);
-
    }
    /**
     * @description 获取一个概率值
@@ -113,3 +165,5 @@ class CpuOverload {
       return Math.random().toFixed(4);
     }
 }
+
+module.exports = CpuOverload;
